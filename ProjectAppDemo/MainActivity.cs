2abdfc7518f999d3a6df0d201a3a1affbde66569
Deszolate-C <deszolate@icloud.com>
2017-06-28 16:01:20 +0700

    Add project files.

---
+++ ProjectAppDemo/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace HappyHealthyCSharp
+{
+    [Activity(Label = "ProjectAppDemo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            SetTheme(Resource.Style.Base_Theme_AppCompat_Light);
+            base.OnCreate(bundle);
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+            SetContentView(Resource.Layout.activity_home);
+        }
+    }
+}
+
