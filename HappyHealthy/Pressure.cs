acd300afb1e5bedb8ff6e6a6947f1006f0ae825b
Deszolate_C <kunvutloveza@hotmail.com>
2017-08-31 10:19:24 +0700

    Pressure Activity

--- HappyHealthy/Pressure.cs
+++ HappyHealthy/Pressure.cs
@@ -16,12 +16,36 @@ namespace HappyHealthyCSharp
     [Activity]
     public class Pressure : Activity
     {
+        private EditText BPLow;
+        private EditText BPUp;
+        private EditText HeartRate;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             SetTheme(Resource.Style.Base_Theme_AppCompat_Light);
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_pressure);
             // Create your application here
+            BPLow = FindViewById<EditText>(Resource.Id.P_costPressureDown);
+            BPUp = FindViewById<EditText>(Resource.Id.P_costPressureTop);
+            HeartRate = FindViewById<EditText>(Resource.Id.P_HeartRate);
+            var save = FindViewById<ImageView>(Resource.Id.imageView29);
+            save.Click += ClickDisLevelsPre;
+        }
+
+        public void ClickDisLevelsPre(View v)
+        {
+            var bpTable = new PressureTABLE();
+            bpTable.InsertPressureToSQL(BPUp.Text, BPLow.Text, HeartRate.Text, 1);
+            GlobalFunction.createDialog(this, $@"Inserted", delegate { this.Finish(); }).Show();
+
+        }
+        public void ClickDisLevelsPre(object sender, EventArgs e)
+        {
+            var bpTable = new PressureTABLE();
+            bpTable.InsertPressureToSQL(BPUp.Text, BPLow.Text, HeartRate.Text, 1);
+            GlobalFunction.createDialog(this, $@"Inserted", delegate { this.Finish(); }).Show();
+
         }
         [Export("ClickBackPreHome")]
         public void ClickBackPreHome(View v)
