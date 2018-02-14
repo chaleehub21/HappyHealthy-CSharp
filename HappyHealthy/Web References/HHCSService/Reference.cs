a3c78c8e1bffc4c83f5883ca9fa9aea022cc7a21
Deszolate <deszolate@icloud.com>
2018-02-14 09:23:01 +0700

    Testing connection mechanics

--- HappyHealthy/Web References/HHCSService/Reference.cs
+++ HappyHealthy/Web References/HHCSService/Reference.cs
@@ -51,7 +51,7 @@ namespace HappyHealthyCSharp.HHCSService {
         
         /// <remarks/>
         public HHCSService() {
-            this.Url = "http://192.168.1.2/HHCSService/HHCSService.asmx";
+            this.Url = "http://172.16.23.82/HHCSService/HHCSService.asmx";
             if ((this.IsLocalFileSystemWebService(this.Url) == true)) {
                 this.UseDefaultCredentials = true;
                 this.useDefaultCredentialsSetExplicitly = false;
